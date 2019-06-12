6c5d52c63c135f89e6f3609829a042aa9384ece3
Balafre78 <47460885+Balafre78@users.noreply.github.com>
2019-06-12 19:13:26 +0200

    Update config.js

--- .gitignore/config.js
+++ .gitignore/config.js
@@ -1,7 +1,3 @@
 exports.PREFIX = '!'
 
 exports.GOOGLE_API_KEY = 'AIzaSyBJNCSHBKmN5EHf2QoHETtbZVtrZTZcEds'
-
-//AIzaSyDmDIoBg73XgDM6TrFWEt0P-PULmaqibPU
-
-//TOKEN : NTQzNTMwMzgyMTM0NDc2ODAx.XMnBGg.f2qCqK1yKVYiAxvzS01br6MvA9c
